954866f4115505e60ef41f0e34e5b46203c4b8f4
Farid Salamanca <jfarid.salamanca@uptc.edu.co>
2022-07-30 10:05:53 -0500

    Añadiendo Login Usuario

--- src/routes.js
+++ src/routes.js
@@ -27,26 +27,29 @@ const routes = [
     component: MainDashboard,
   },
   {
-    name: "Crear dispositivos",
-    layout: "/admin",
-    icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
-    path: "/data-tables",
-    component: DataTables,
-  },
-  {
     name: "Iniciar sesion",
     layout: "/auth",
     path: "/sign-in",
     icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
     component: SignInCentered,
   },
-  {
+
+
+];
+const user = 'admin';
+if (user == 'admin') {
+  routes.push({
     name: "Crear Cuenta",
     layout: "/auth",
     path: "/sign-up",
     component: SignUpCentered,
-  }
-
-];
-
+  },
+    {
+      name: "Crear dispositivos",
+      layout: "/admin",
+      icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
+      path: "/data-tables",
+      component: DataTables,
+    })
+}
 export default routes;
